42e48f2a5df15007bcfbc7edbd5863a1e05d29e1
jamclean23 <jamclean23@gmail.com>
2022-11-15 10:54:52 -0500

    Fix multiple event listener assignment, fix move up button on card

--- src/cards.js
+++ src/cards.js
@@ -1,76 +1,76 @@
 //Functions related to task cards
 
+let savedCards = [];
 
 //Takes an object containing title, description, and due date
 function add(modalInfo) {
-    const emptyCard = `<div class="card workingCard">
-        <button class="upArrow">/\\</button>
+    const emptyCardContents = `<button class="upArrow">/\\</button>
         <button class="downArrow">\\/</button>
         <h3 class="title"></h3>
         <div class="created">Created:</div>
         <div class="dueDate">Due:</div>
         <p class="description"></p>
         <button class="xButton">X</button>
-        <button class="checkButton">✓</button>
-        </div>`
+        <button class="checkButton">✓</button>`
+    const newCard = document.createElement('div');
+    newCard.innerHTML = emptyCardContents;
+    newCard.classList.add('card');
     const main = document.querySelector('main');
-    main.innerHTML += emptyCard;
-    const workingCard = document.querySelector('.workingCard');
-    updateEventListeners(workingCard);
-    workingCard.querySelector('.title').innerText = modalInfo.title;
-    workingCard.querySelector('.description').innerText = modalInfo.description;
-    workingCard.querySelector('.created').innerText += "\n" + modalInfo.created;
-    workingCard.querySelector('.dueDate').innerText += "\n" + modalInfo.date;
-    workingCard.classList.remove('workingCard');
+    const firstCard = main.querySelector('.card');
+    if (firstCard) {
+        main.appendChild(newCard);
+    } else {
+        main.appendChild(newCard);
+    }
+    updateEventListeners(newCard);
+    newCard.querySelector('.title').innerText = modalInfo.title;
+    newCard.querySelector('.description').innerText = modalInfo.description;
+    newCard.querySelector('.created').innerText += "\n" + modalInfo.created;
+    newCard.querySelector('.dueDate').innerText += "\n" + modalInfo.date;
+    newCard.classList.remove('newCard');
+    savedCards = save();
 }
 
 function remove(event) {
     const main = document.querySelector('main');
-    console.log(main);
     main.removeChild(event.parentElement);
+    savedCards = save();
 }
 
-function clear() {
-
-}
-
-function updateEventListeners() {
+function updateEventListeners(card) {
     const main = document.querySelector('main');
-    const cards = document.querySelectorAll('.card');
-    cards.forEach((card)=>{
-        //"Remove" button
-        const xButton = card.querySelector('.xButton');
-        xButton.addEventListener('click', (event)=>{
-            remove(event.target);
-        })
-        //"Move up" button
-        const upButton = card.querySelector('.upArrow');
-        upButton.addEventListener('click', (event)=>{
-            if (event.target.previousSibling) {
-                const previousSibling = event.target.parentElement.previousSibling;
-                main.insertBefore(card, previousSibling);
-            }
-        });
-        //"Move down" button
-        const downButton = card.querySelector('.downArrow');
-        downButton.addEventListener('click', (event)=>{
-            if (event.target.parentElement.nextSibling) {
-                const nextNextSibling = event.target.parentElement.nextSibling.nextSibling;
-                if (nextNextSibling) {
-                    main.insertBefore(card, nextNextSibling);
-                } else {
-                    main.appendChild(card);
-                }
-            }
-        });
-        //"Completed" button
-        const completeButton = card.querySelector('.checkButton');
-        completeButton.addEventListener('click', ()=>{
-            card.classList.toggle('completed');
-            sortCompletedCards();
-        });
-
+    //"Remove" button
+    const xButton = card.querySelector('.xButton');
+    xButton.addEventListener('click', (event)=>{
+        remove(event.target);
+    })
+    //"Move up" button
+    const upButton = card.querySelector('.upArrow');
+    upButton.addEventListener('click', (event)=>{
+        if (event.target.parentElement.previousSibling) {
+            const previousSibling = event.target.parentElement.previousSibling;
+            main.insertBefore(card, previousSibling);
+        }
     });
+    //"Move down" button
+    const downButton = card.querySelector('.downArrow');
+    downButton.addEventListener('click', (event)=>{
+        if (event.target.parentElement.nextSibling) {
+            const nextNextSibling = event.target.parentElement.nextSibling.nextSibling;
+            if (nextNextSibling) {
+                main.insertBefore(card, nextNextSibling);
+            } else {
+                main.appendChild(card);
+            }
+        }
+    });
+    //"Completed" button
+    const completeButton = card.querySelector('.checkButton');
+    completeButton.addEventListener('click', ()=>{
+        card.classList.toggle('completed');
+        sortCompletedCards();
+    });
+    savedCards = save();
 }
 
 function sortCompletedCards() {
@@ -87,11 +87,31 @@ function sortCompletedCards() {
         const main = document.querySelector('main');
         main.appendChild(card);
     });
+    savedCards = save();
+}
+
+function save() {
+    //Store card to an array to be generated on next load
+    const cards = [...document.querySelectorAll('.card')];
+    //build object to be stored
+    cards.forEach((card)=>{
+        //title
+        //description
+        //date
+        //comparedDueDate
+    });
+    return cards;
+
+}
+
+function viewSaved() {
+    console.log(savedCards);
+    return savedCards;
 }
 
 export {
     add,
     remove,
-    clear,
     updateEventListeners,
+    viewSaved,
 }
--- src/index.js
+++ src/index.js
@@ -24,3 +24,12 @@ submitButton.addEventListener('click', ()=>{
     Modal.hide();
     Modal.clear();
 });
+
+const devButton = document.createElement('button');
+devButton.innerText = "test";
+devButton.style.cssText = "position: fixed; top: 1rem; left: 1rem; padding: .5rem 1rem";
+document.body.appendChild(devButton);
+devButton.addEventListener('click', ()=>{
+    const savedCards = localStorage.getItem("savedCards");
+    console.log(JSON.parse(savedCards));
+});
